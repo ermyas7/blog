ec361beb82ff591f8e7323dad8a4030f0a0fd44e
Ermyas Fekadu <ermyas.fekadu@gmail.com>
2018-02-14 20:15:33 +0530

    add query and implement archives dynamically

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Post;
 use App\User;
+use Carbon\Carbon;
 
 class PostsController extends Controller
 {
@@ -16,8 +17,27 @@ class PostsController extends Controller
 
     public function index()
     
-    {	$posts = Post::latest()->get();
-    	return view('posts.index', compact('posts'));
+    {	$posts = Post::latest();
+
+
+      if($month =request('month')){
+            $posts->whereMonth('created_at', Carbon::parse($month)->month);
+        }
+
+        if($year = request('year')){
+            $posts->whereYear('created_at', $year);
+        }
+
+        $posts = $posts->get();
+
+        $archives = Post::selectRaw('year(created_at) year, monthname(created_at) month, count(*) published')
+        ->groupBy('year', 'month')
+        ->orderByRaw('min(created_at) desc')
+        ->get()
+        ->toArray();
+
+
+    	return view('posts.index', compact('posts', 'archives'));
     }
 
     public function show(Post $post)
@@ -38,15 +58,12 @@ class PostsController extends Controller
     	$this->validate(request(),[
     	'title' => 'required',
     	'body' => 'required',
-]);     
-
-        Post::create([
-
-            'title' => request('title'),
-            'body' => request('body'),
-            'user_id' => auth()->id()
-        ]);       
+                                            ]);     
+         auth()-user()->publish(
+            new Post(request(['title', 'body']))
+                                                );
 
     	    	return redirect('/');
     }
+
 }
